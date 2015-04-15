49defaba505668c43dff82d94e0e37d93c652ad7
Aisling Guinan <sweetabsinthekid@gmail.com>
2015-04-15 10:54:14 +0100

    Added back files for log in, ect
    still not working, but an update for who's looking

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -29,3 +29,37 @@ def list(request):
         {'documents': documents, 'form': form},
         context_instance=RequestContext(request)
     )
+
+def welcome(request):
+    return render(request,'welcome.html')
+
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+    'form': form
+    })
+
+    return render_to_response(
+    'registration/register.html',
+    variables,
+    )
+
+def register_success(request):
+    return render_to_response(
+    'registration/success.html',
+    )
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')	
--- myproject/urls.py
+++ myproject/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.contrib.auth.views import login, logout
+from myapp.views import register,welcome
 
 urlpatterns = patterns('',
 	(r'^myapp/', include('myproject.myapp.urls')),
-	(r'^$', RedirectView.as_view(url='/myapp/list/')), # Just for ease of use.
+	url(r'^$',welcome),
+	url(r'^accounts/login/$',login),
+    url(r'^accounts/logout/$',logout),
+    url(r'^accounts/register/$',register),
+	(r'^upload/$', RedirectView.as_view(url='/myapp/list/')), # Just for ease of use.
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
