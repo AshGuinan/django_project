94168f72c73454aa940ea656f49e76d0311da213
Aisling Guinan <sweetabsinthekid@gmail.com>
2015-04-15 15:41:00 +0100

    Bugged Code

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,10 +7,10 @@ from django.contrib.auth.views import login, logout
 from myapp.views import register,welcome
 
 urlpatterns = patterns('',
-	(r'^myapp/', include('myproject.myapp.urls')),
-	url(r'^$',welcome),
+	url(r'^$', welcome),
 	url(r'^accounts/login/$',login),
     url(r'^accounts/logout/$',logout),
     url(r'^accounts/register/$',register),
+	(r'^myapp/', include('myproject.myapp.urls')),
 	(r'^upload/$', RedirectView.as_view(url='/myapp/list/')), # Just for ease of use.
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
