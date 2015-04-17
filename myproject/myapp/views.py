35107493dcf120d11f38f914aaa8448be2aca0e1
Aisling Guinan <sweetabsinthekid@gmail.com>
2015-04-17 16:00:35 +0100

    :boom: Demo Code

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
 from myproject.myapp.models import Document
 from myproject.myapp.forms import DocumentForm
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+
+
 
 def list(request):
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
 
             # Redirect to the document list after POST
             return HttpResponseRedirect(reverse('myproject.myapp.views.list'))
     else:
-        form = DocumentForm() # A empty, unbound form
+        form = DocumentForm()  # A empty, unbound form
 
     # Load documents for the list page
     documents = Document.objects.all()
@@ -31,35 +36,18 @@ def list(request):
     )
 
 def welcome(request):
-    return render(request,'welcome.html')
+    return render(request, 'welcome.html')
 
-@csrf_protect
 def register(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email']
-            )
-            return HttpResponseRedirect('/register/success/')
+            new_user = form.save()
+            return HttpResponseRedirect("/myapp/list/")
     else:
-        form = RegistrationForm()
-    variables = RequestContext(request, {
-    'form': form
-    })
+        form=UserCreationForm()
 
-    return render_to_response(
-    'registration/register.html',
-    variables,
-    )
+    return render(request,"registration/register.html",{'form':form})
 
-def register_success(request):
-    return render_to_response(
-    'registration/success.html',
-    )
 
-def logout_page(request):
-    logout(request)
-    return HttpResponseRedirect('/')	
+	
--- myproject/urls.py
+++ myproject/urls.py
@@ -5,12 +5,17 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from django.contrib.auth.views import login, logout
 from myapp.views import register,welcome
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', welcome),
-	url(r'^accounts/login/$',login),
-    url(r'^accounts/logout/$',logout),
-    url(r'^accounts/register/$',register),
-	(r'^myapp/', include('myproject.myapp.urls')),
-	(r'^upload/$', RedirectView.as_view(url='/myapp/list/')), # Just for ease of use.
+	url(r'^login/$',login),
+	url(r'^logout/$',logout),
+	url(r'^register/$',register),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^accounts/profile/$',welcome),
+(r'^myapp/', include('myproject.myapp.urls')),
+	(r'^upload/', RedirectView.as_view(url='/myapp/list/')), # Just for ease of use.
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
